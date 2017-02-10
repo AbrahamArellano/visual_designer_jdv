92980ded983effef38271935b00f0835a93fc873
aarellan <aarellan@localhost.localdomain>
2017-02-10 14:35:47 +0100

    - Rename of components

--- visual_designer_jdv/data/data.sql
+++ visual_designer_jdv/data/data.sql
@@ -1,62 +1,62 @@
 DROP TABLE IF EXISTS person;
-DROP TABLE IF EXISTS KapitaldienstDetailsSummeGrundlagen;
-DROP TABLE IF EXISTS ISvzEintrag;
+DROP TABLE IF EXISTS Kapital;
+DROP TABLE IF EXISTS Eintrag;
 
 
---  private String fPersNr = "";
---  private String fFamilienstand = "";
---  private String fKundenNummer = "";
---  private String fVorname = "";
+--  private String personNr = "";
+--  private String familienstand = "";
+--  private String kundenNummer = "";
+--  private String vorname = "";
 CREATE TABLE person
 (
-  fpersNr varchar(50) not null,
-  fFamilienstand varchar(50) not null,
-  fKundenNummer varchar(50) not null,
-  fVorname varchar(50) not null,
-  PRIMARY KEY (fpersNr)
+  personNr varchar(50) not null,
+  familienstand varchar(50) not null,
+  kundenNummer varchar(50) not null,
+  vorname varchar(50) not null,
+  PRIMARY KEY (personNr)
 );
 
---  protected Short kapdBergGrdlLfdn;
---  protected Short kapdBergGrdlRang;
---  protected BigDecimal kapdKdrgFreiSum;
-CREATE TABLE KapitaldienstDetailsSummeGrundlagen
+--  protected Short kapital1;
+--  protected Short kapital2;
+--  protected BigDecimal kapital3;
+CREATE TABLE Kapital
 (
-   fpersNr varchar(50) not null,
-   kapdBergGrdlLfdn integer,
-   kapdBergGrdlRang integer,
-   kapdKdrgFreiSum integer       
+   personNr varchar(50) not null,
+   kapital1 integer,
+   kapital2 integer,
+   kapital3 integer       
 );
 
-CREATE TABLE ISvzEintrag
+CREATE TABLE Eintrag
 (
-   fpersNr varchar(50) not null,
-   svzId varchar(50) not null,
-   svzDb varchar(50) not null,
+   personNr varchar(50) not null,
+   specialId varchar(50) not null,
+   dbId varchar(50) not null,
    kurzanzeige varchar(50) not null       
 );
 
 -- person
-INSERT INTO person (fpersNr, fFamilienstand, fKundenNummer, fVorname) VALUES ('101','LEDIG','01','John Doe');
-INSERT INTO person (fpersNr, fFamilienstand, fKundenNummer, fVorname) VALUES ('102','VERHEIRATET','02','Max Muster');
-INSERT INTO person (fpersNr, fFamilienstand, fKundenNummer, fVorname) VALUES ('103','VERHEIRATET','03','Test User');
-INSERT INTO person (fpersNr, fFamilienstand, fKundenNummer, fVorname) VALUES ('104','GESCHIEDEN','04','Thomas W');
+INSERT INTO person (personNr, familienstand, kundenNummer, vorname) VALUES ('101','LEDIG','01','John Doe');
+INSERT INTO person (personNr, familienstand, kundenNummer, vorname) VALUES ('102','VERHEIRATET','02','Max Muster');
+INSERT INTO person (personNr, familienstand, kundenNummer, vorname) VALUES ('103','VERHEIRATET','03','Test User');
+INSERT INTO person (personNr, familienstand, kundenNummer, vorname) VALUES ('104','GESCHIEDEN','04','Thomas W');
 
--- KapitaldienstDetailsSummeGrundlagen
-INSERT INTO KapitaldienstDetailsSummeGrundlagen 
-(fpersNr, kapdBergGrdlLfdn, kapdBergGrdlRang, kapdKdrgFreiSum) VALUES ('101',333,133,100);
-INSERT INTO KapitaldienstDetailsSummeGrundlagen 
-(fpersNr, kapdBergGrdlLfdn, kapdBergGrdlRang, kapdKdrgFreiSum) VALUES ('102',444,144,100);
-INSERT INTO KapitaldienstDetailsSummeGrundlagen 
-(fpersNr, kapdBergGrdlLfdn, kapdBergGrdlRang, kapdKdrgFreiSum) VALUES ('103',555,155,100);
-INSERT INTO KapitaldienstDetailsSummeGrundlagen 
-(fpersNr, kapdBergGrdlLfdn, kapdBergGrdlRang, kapdKdrgFreiSum) VALUES ('101',333,133,100);
-INSERT INTO KapitaldienstDetailsSummeGrundlagen 
-(fpersNr, kapdBergGrdlLfdn, kapdBergGrdlRang, kapdKdrgFreiSum) VALUES ('102',444,144,100);
-INSERT INTO KapitaldienstDetailsSummeGrundlagen 
-(fpersNr, kapdBergGrdlLfdn, kapdBergGrdlRang, kapdKdrgFreiSum) VALUES ('103',555,155,100);
+-- Kapital
+INSERT INTO Kapital 
+(personNr, kapital1, kapital2, kapital3) VALUES ('101',333,133,100);
+INSERT INTO Kapital 
+(personNr, kapital1, kapital2, kapital3) VALUES ('102',444,144,100);
+INSERT INTO Kapital 
+(personNr, kapital1, kapital2, kapital3) VALUES ('103',555,155,100);
+INSERT INTO Kapital 
+(personNr, kapital1, kapital2, kapital3) VALUES ('101',333,133,100);
+INSERT INTO Kapital 
+(personNr, kapital1, kapital2, kapital3) VALUES ('102',444,144,100);
+INSERT INTO Kapital 
+(personNr, kapital1, kapital2, kapital3) VALUES ('103',555,155,100);
 
--- ISvzEintrag
-INSERT INTO ISvzEintrag (fpersNr, svzId, svzDb, kurzanzeige) VALUES ('101','200-100','DB01','A1');
-INSERT INTO ISvzEintrag (fpersNr, svzId, svzDb, kurzanzeige) VALUES ('102','210-100','DB02','A2');
-INSERT INTO ISvzEintrag (fpersNr, svzId, svzDb, kurzanzeige) VALUES ('103','220-100','DB03','A3');
+-- Eintrag
+INSERT INTO Eintrag (personNr, specialId, dbId, kurzanzeige) VALUES ('101','200-100','DB01','A1');
+INSERT INTO Eintrag (personNr, specialId, dbId, kurzanzeige) VALUES ('102','210-100','DB02','A2');
+INSERT INTO Eintrag (personNr, specialId, dbId, kurzanzeige) VALUES ('103','220-100','DB03','A3');
 
