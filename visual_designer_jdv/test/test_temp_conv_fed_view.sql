04a8769d790538cf90a6c63fb01a21f1db237f29
aarellan <aarellan@localhost.localdomain>
2017-02-20 07:42:20 +0100

    - Added new temperature converter using user defined functions

---
+++ visual_designer_jdv/test/test_temp_conv_fed_view.sql
@@ -0,0 +1,4 @@
+// 1) Enter valid SQL query (i.e. SELECT * FROM FOO)
+// 2) Highlight query and right-click select "Execute Selected Text"
+// Query that test the "TempConvFedView" service and the user defined function view.
+SELECT * FROM temperature1;
